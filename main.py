e1ee2a23636e4d69f49b5cbb12c5f471fadca4e5
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2023-12-28 17:31:41 +0000

    szenvedés és próbálkozás

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-with open("osvenyek.txt", "r", encoding="utf-8") as file:
+"""with open("osvenyek.txt", "r", encoding="utf-8") as file:
     osvenyeklist = [i.strip() for i in file]
 
 
@@ -30,7 +30,7 @@ while  not (1 <= (osv := int(input("Adja meg egy ösvény sorszámát!(1-50): ")
 
 while not (2 <= int(input("Adja meg a játékosok számát!(2-5): ")) <= 5):
     print("2 és 5 között adj meg sorszámot!")
-print
+print()
 
 
 print("5. feladat")
@@ -38,7 +38,30 @@ statisztika = {i:osvenyeklist[osv-1].count(i) for i in osvenyeklist[osv-1]}
 [print(f"{k}: {v} darab") for k, v in statisztika.items()]
 
 
-"""with open("kulonleges.txt", "w", encoding="utf-8") as file:
-    for i in osvenyeklist[osv-1]:
-        file.read("KURVAANYD")"""
+"""
 
+osveny = "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+dobas = [2, 4, 6, 5, 6, 1, 4, 1, 3, 4, 5, 1, 2, 4, 6, 2, 3, 3, 5, 5, 6, 2, 1, 3, 4, 4, 2, 6, 2 ,4, 5, 6, 3, 2, 1, 2, 3, 1, 1,6 ,4, 1 ,4 ,4 ,3 ,6 ,1 ,4 ,2 ,6, 2 ,4, 5 ,5 ,1, 4 ,5, 3 ,4 ,6 ,6, 5, 2, 2, 4,1, 1 ,5 ,5 ,5, 2]
+osvenyhossza = len(osveny)
+jatekosokszama = 3
+adatok = [
+    ["korokszama", 0]
+
+]
+
+
+for i in range(jatekosokszama):
+    adatok.append(
+        {
+            "dobasosszeg": 0,
+            "dobasok": []
+        }
+    )
+
+kilep = True
+while kilep:
+    for i in range(jatekosokszama):
+
+        if adatok[i]["dobasosszeg"].append(dobas[adatok[i]["dobasosszeg"])]) > osvenyhossza:
+              adatok[i]["dobasosszeg"].append(dobas.index(adatok[i]["dobasosszeg"]))
+              kilep = False
